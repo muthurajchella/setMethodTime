bb8c1ada1873c7ee82d7914853984b95a9f33681
Muthurajchella- <muthuraj.chella@gmail.com>
2022-12-15 14:44:08 +0530

    setMethods

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var time = new Date();
+time.setTime(1);
+document.getElementById("para").innerHTML = time;
+
+var time = new Date();
+time.setFullYear(1997);
+document.getElementById("para").innerHTML = time;
+
+var time = new Date();
+time.setFullYear(1997,2,1);
+document.getElementById("para").innerHTML = time;
+
+var time = new Date();
+time.setMonth(2);
+document.getElementById("para").innerHTML = time;
+
+var time = new Date();
+time.setDate(2);
+document.getElementById("para").innerHTML = time;
+
+var time = new Date();
+time.setHours(1);
+document.getElementById("para").innerHTML = time;
+
+
